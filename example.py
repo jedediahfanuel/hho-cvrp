c26b4e9c46625c1c2716a311f4c3fc8248bfea15
Jeddi212 <jedediahf51@gmail.com>
2023-01-18 16:23:01 +0700

    🌵 :: rid exa

--- example.py
+++ example.py
@@ -8,12 +8,9 @@ Created on Tue May 17 15:50:25 2016
 from optimizer import run
 
 # Select optimizers
-# "SSA","PSO","GA","BAT","FFA","GWO","WOA","MVO","MFO","CS","HHO","SCA","JAYA","DE"
-optimizer = ["SSA", "PSO", "GWO"]
+optimizer = ["HHO"]
 
 # Select benchmark function"
-# "F1","F2","F3","F4","F5","F6","F7","F8","F9","F10","F11","F12","F13","F14","F15","F16","F17","F18","F19"
-# "Ca1","Ca2","Gt1","Mes","Mef","Sag","Tan","Ros"
 objectivefunc = ["F3", "F4"]
 
 # Select number of repetitions for each experiment.
