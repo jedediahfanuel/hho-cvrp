48e46f9f9ecbcf7a95168ae14a10ff3b39421df6
Jeddi212 <jedediahf51@gmail.com>
2023-01-20 13:59:23 +0700

    🥶 :: biasalah masih coba coba

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-from pprint import pprint
-
-import cvrplib
-
-# Download instances
-instance = cvrplib.download('A-n32-k5')
-instance, solution = cvrplib.download('A-n32-k5', solution=True)
-
-pprint(instance)
-pprint(solution)
+# from pprint import pprint
+#
+# import cvrplib
+#
+# # Download instances
+# instance = cvrplib.download('A-n32-k5')
+# instance, solution = cvrplib.download('A-n32-k5', solution=True)
+#
+# pprint(instance)
+# pprint(solution)
 #
 # import random
 # import time
@@ -19,24 +19,24 @@ pprint(solution)
 # print(random.sample(range(n), n))
 
 # initialize the location and Energy of the rabbit
-# import numpy
-#
-# dim = 10
-# lb = 0
-# ub = 1
-# s = 5
-#
-# Rabbit_Location = numpy.zeros(dim)
-# Rabbit_Energy = float("inf")  # change this to -inf for maximization problems
-#
-# if not isinstance(lb, list):
-#     lb = [lb for _ in range(dim)]
-#     ub = [ub for _ in range(dim)]
-#
-# lb = numpy.asarray(lb)
-# ub = numpy.asarray(ub)
-#
-# # Initialize the locations of Harris' hawks
-# X = numpy.asarray([x * (ub - lb) + lb for x in numpy.random.uniform(0, 1, (s, dim))])
-#
-# print(X)
+import numpy
+
+dim = 9
+lb = 0
+ub = 1
+s = 5
+
+Rabbit_Location = numpy.zeros(dim)
+Rabbit_Energy = float("inf")  # change this to -inf for maximization problems
+
+if not isinstance(lb, list):
+    lb = [lb for _ in range(dim)]
+    ub = [ub for _ in range(dim)]
+
+lb = numpy.asarray(lb)
+ub = numpy.asarray(ub)
+
+# Initialize the locations of Harris' hawks
+X = numpy.asarray([x * (ub - lb) + lb for x in numpy.random.uniform(0, 1, (s, dim))])
+
+print(X)
