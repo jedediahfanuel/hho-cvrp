85742b9239e1d0c10f9176b51bac86718f8689c4
Jeddi212 <jedediahf51@gmail.com>
2023-01-23 23:42:02 +0700

    🗂 :: example using wvrplib

--- example.py
+++ example.py
@@ -4,14 +4,14 @@ from optimizer import run
 optimizer = ["HHO"]
 
 # Select benchmark function
-objectivefunc = ["F3", "F4"]
+objectivefunc = ["A-n32-k5"]
 
 # Select number of repetitions for each experiment.
 # To obtain meaningful statistical results, usually 30 independent runs are executed for each algorithm.
-NumOfRuns = 3
+NumOfRuns = 1
 
 # Select general parameters for all optimizers (population size, number of iterations) ....
-params = {"PopulationSize": 30, "Iterations": 50}
+params = {"PopulationSize": 5, "Iterations": 1}
 
 # Choose whether to Export the results in different formats
 export_flags = {
