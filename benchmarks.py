43e2acbee03da8d1b7c75043ce49ade4b88db1d5
Jeddi212 <jedediahf51@gmail.com>
2023-01-27 13:32:46 +0700

    🧂 :: bring back lb ub dim

--- benchmarks.py
+++ benchmarks.py
@@ -1,7 +1,4 @@
-from typing import Dict, Any, List
-
 import numpy
-import math
 import cvrplib
 
 
@@ -9,9 +6,10 @@ def getFunctionDetails(a):
     # Download instances
     instance = cvrplib.download(a)
 
-    # [name, lb, ub, problem]
+    # [name, lb, ub, dim]
     param = {
-        "cvrp": ["cvrp", 0, 1, instance],
+        # dimensi = banyaknya customer
+        "cvrp": ["cvrp", 1, instance.dimension - 0.01, instance.n_customers],
     }
     return param.get("cvrp", "nothing")
 
@@ -29,12 +27,13 @@ def cvrp(routes, distances):
 def generate_stable_solution(solution, lb=None, ub=None):
     # print(f"Raw: {solution}")
     ## Bring them back to boundary
-    solution = np.clip(solution, lb, ub)
+    # sudah dilakuin sebelum masuk fungsi objektif
+    # solution = numpy.clip(solution, lb, ub)
 
     solution_set = set(list(range(0, len(solution))))
-    solution_done = np.array([-1, ] * len(solution))
+    solution_done = numpy.array([-1, ] * len(solution))
     solution_int = solution.astype(int)
-    city_unique, city_counts = np.unique(solution_int, return_counts=True)
+    city_unique, city_counts = numpy.unique(solution_int, return_counts=True)
 
     ### Way 1: Stable, not random
     for idx, city in enumerate(solution_int):
@@ -42,7 +41,7 @@ def generate_stable_solution(solution, lb=None, ub=None):
             continue
         if city in city_unique:
             solution_done[idx] = city
-            city_unique = np.where(city_unique == city, -1, city_unique)
+            city_unique = numpy.where(city_unique == city, -1, city_unique)
         else:
             list_cities_left = list(solution_set - set(city_unique) - set(solution_done))
             # print(list_cities_left)
@@ -53,15 +52,15 @@ def generate_stable_solution(solution, lb=None, ub=None):
 
 def generate_unstable_solution(solution, lb=None, ub=None):
     # print(solution)
-    solution_bound = np.clip(solution, lb, ub)
+    solution_bound = numpy.clip(solution, lb, ub)
     solution_set = set(range(int(lb[0]), round(ub[0])))
-    solution_done = np.array([-1, ] * len(solution_bound))
+    solution_done = numpy.array([-1, ] * len(solution_bound))
     solution_int = solution_bound.astype(int)
     # print(solution_int)
-    city_unique, city_counts = np.unique(solution_int, return_counts=True)
+    city_unique, city_counts = numpy.unique(solution_int, return_counts=True)
 
     ## Way 2: Random, not stable
-    # count_dict = dict(zip(*np.unique(solution_int, return_counts=True)))
+    # count_dict = dict(zip(*numpy.unique(solution_int, return_counts=True)))
     count_dict = dict(zip(city_unique, city_counts))
     for idx, city in enumerate(solution_int):
         if solution_done[idx] != -1:
@@ -71,14 +70,14 @@ def generate_unstable_solution(solution, lb=None, ub=None):
                 if count_dict[city] == 1:
                     solution_done[idx] = city
                 else:
-                    idx_list_city = np.where(solution_int == city)[0]
-                    idx_city_keep = np.random.choice(idx_list_city)
+                    idx_list_city = numpy.where(solution_int == city)[0]
+                    idx_city_keep = numpy.random.choice(idx_list_city)
                     solution_done[idx_city_keep] = city
                     if idx_city_keep != idx:
-                        solution_done[idx] = np.random.choice(list(solution_set - set(solution_done) - set(city_unique)))
+                        solution_done[idx] = numpy.random.choice(list(solution_set - set(solution_done) - set(city_unique)))
             else:
-                solution_done[idx] = np.random.choice(list(solution_set - set(solution_done) - set(city_unique)))
+                solution_done[idx] = numpy.random.choice(list(solution_set - set(solution_done) - set(city_unique)))
         else:
-            solution_done[idx] = np.random.choice(list(solution_set - set(solution_done) - set(city_unique)))
+            solution_done[idx] = numpy.random.choice(list(solution_set - set(solution_done) - set(city_unique)))
     # print(solution_done)
     return solution_done
