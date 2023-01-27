41b3e02ec961bffc799f232d31abe132a56f487e
Jeddi212 <jedediahf51@gmail.com>
2023-01-27 15:12:18 +0700

    🔺 :: increment some vars

--- example.py
+++ example.py
@@ -8,10 +8,10 @@ objectivefunc = ["A-n32-k5"]
 
 # Select number of repetitions for each experiment.
 # To obtain meaningful statistical results, usually 30 independent runs are executed for each algorithm.
-NumOfRuns = 1
+NumOfRuns = 30
 
 # Select general parameters for all optimizers (population size, number of iterations) ....
-params = {"PopulationSize": 5, "Iterations": 3}
+params = {"PopulationSize": 20, "Iterations": 50}
 
 # Choose whether to Export the results in different formats
 export_flags = {
