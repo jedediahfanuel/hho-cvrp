181fceec441f191b9e877c94bb68f77909085c63
Jeddi212 <jedediahf51@gmail.com>
2023-01-31 00:26:53 +0700

    👩🏼‍🔧 :: remove unused func

--- plot_scatter.py
+++ plot_scatter.py
@@ -60,36 +60,3 @@ def plot_cities(s: Solution, filename: str, pathsave: str, exts=(".png", ".pdf")
     if platform.system() != "Linux":
         plt.show()
     plt.close()
-
-
-def plot_solutions(self, dict_solutions, filename: str, pathsave: str, exts=(".png", ".pdf"),
-                   size=100, show_id=True):
-    x_min, x_max, y_min, y_max, text_space_x, text_space_y, space_x, space_y = self.__get_space__()
-
-    for idx_pos, solution in enumerate(dict_solutions.values()):
-        obj_value = solution[1]
-        city_coord = self.city_positions[solution[0]]
-        line_x = city_coord[:, 0]
-        line_y = city_coord[:, 1]
-        plt.scatter(self.city_positions[:, 0].T, self.city_positions[:, 1].T, s=size, c='k')
-
-        # add text annotation
-        if show_id:
-            for city in range(0, self.n_cities):
-                plt.text(self.city_positions[city][0] + text_space_x, self.city_positions[city][1] - text_space_y,
-                         f"{city}", size='medium', color='black', weight='semibold')
-
-        plt.plot(line_x.T, line_y.T, 'r-')
-        plt.text(x_min - 2 * space_x, y_min - 2 * space_y, f"Total distance: {obj_value:.2f}",
-                 fontdict={'size': 12, 'color': 'red'})
-        plt.xlim((x_min - space_x, x_max + space_x))
-        plt.ylim((y_min - space_y, y_max + space_y))
-        plt.title(f"Solution: {idx_pos + 1}, GBest: {solution[1]}")
-
-        Path(pathsave).mkdir(parents=True, exist_ok=True)
-
-        for idx, ext in enumerate(exts):
-            plt.savefig(f"{pathsave}/{filename}-id{idx_pos + 1}{ext}", bbox_inches='tight')
-        if platform.system() != "Linux":
-            plt.show()
-        plt.close()
