a8b5e44b137b473c657726b9667850ff9083388b
Jeddi212 <jedediahf51@gmail.com>
2023-02-03 23:25:45 +0700

    🛬 :: two opt func

--- two_opt.py
+++ two_opt.py
@@ -1,5 +1,26 @@
 def two_opt(route, distances):
-    pass
+    """
+    Two opt is a heuristic method that swap two edges in a graph.
+    It iterates over the path and check if the swap is feasible,
+    then swap the sub-route, otherwise continue iterates.
+
+    :param route: one dimensional array-like
+    :param distances: matrix of distances
+    :return: the array-like that have been processed
+    """
+    improved = True
+    while improved:
+        improved = False
+        for i in range(len(route) - 2):
+            for j in range(i + 2, len(route)):
+                len_delta = - distances(route[i][j + 1]) - distances(route[j][j + 1]) \
+                            + distances(route[i + 1][j + 1]) + distances(route[i][j])
+
+                if len_delta < 0:
+                    route = swap(route, i, j)
+                    improved = True
+
+    return route
 
 
 def swap(r, i, j):
