9a1e7c9ce3e82029a3fa9e8455afb0948557478f
Jeddi212 <jedediahf51@gmail.com>
2023-02-04 00:49:11 +0700

    🌈 :: exclude depot from two_opt

--- optimizers/two_opt.py
+++ optimizers/two_opt.py
@@ -14,7 +14,7 @@ def two_opt(route, distances):
         improved = False
         print(route)
         for i in range(len(route) - 2):
-            for j in range(i + 2, len(route)):
+            for j in range(i + 2, len(route) - 1):
                 len_delta = - distances[i][i + 1] - distances[j][j + 1] \
                             + distances[i + 1][j + 1] + distances[i][j]
 
@@ -28,14 +28,14 @@ def two_opt(route, distances):
 def swap(r, i, j):
     """
     Swap two edges, or inverse the middle route.
-    example: i, j = 1, 3
-    A - B - C - D - E
-    A - D - C - B - E
+    example: i, j = 1, 4
+    A - B - E - D - C - F - G - H - A
+    A - B - C - D - E - F - G - H - A
 
     :param r: one dimensional array-like
     :param i: first point (index)
     :param j: second point (index)
     :return: one dimensional array-like after inversion
     """
-    r[i:j+1] = r[j:i-1:-1] if i != 0 else r[j::-1]
+    r[i+1:j+1] = r[j:i:-1]
     return r
