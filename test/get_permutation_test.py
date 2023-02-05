eae047ec3aeb8293e1f6f30b2fa3e62f79ec01c8
Jeddi212 <jedediahf51@gmail.com>
2023-02-05 18:58:55 +0700

    💱 :: get permutation unit test

--- test/get_permutation_test.py
+++ test/get_permutation_test.py
@@ -1,8 +1,19 @@
 import unittest
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+from optimizers.HHO import get_permutation
+
+
+class GetPermutationTest(unittest.TestCase):
+    def setUp(self):
+        self.arr = [0.1, 0.5, 0.3, 0.7, 0.2]
+        self.res = [0, 4, 2, 1, 3]
+
+    def test_get_permutation(self):
+        self.assertEqual(
+            self.res,
+            get_permutation(self.arr)
+        )
+
 
 if __name__ == '__main__':
     unittest.main()
