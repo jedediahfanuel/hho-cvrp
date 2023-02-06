cddf0723b2a59b6476f7da98d9ae019f2224536e
Jeddi212 <jedediahf51@gmail.com>
2023-02-06 17:06:25 +0700

    🍪 :: rename swap -> inverse

--- test/swap_test.py
+++ test/swap_test.py
@@ -1,28 +1,28 @@
 import unittest
 
-from optimizers.two_opt import swap
+from optimizers.two_opt import inverse
 
 
-class SwapCase(unittest.TestCase):
+class InverseCase(unittest.TestCase):
     def setUp(self):
         self.route = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-    def test_head_swap(self):
+    def test_head_inverse(self):
         self.assertEqual(
             [0, 4, 3, 2, 1, 5, 6, 7, 8, 9],
-            swap(self.route, 0, 4)
+            inverse(self.route, 0, 4)
         )
 
-    def test_body_swap(self):
+    def test_body_inverse(self):
         self.assertEqual(
             [0, 1, 2, 6, 5, 4, 3, 7, 8, 9],
-            swap(self.route, 2, 6)
+            inverse(self.route, 2, 6)
         )
 
-    def test_tail_swap(self):
+    def test_tail_inverse(self):
         self.assertEqual(
             [0, 1, 2, 3, 4, 5, 9, 8, 7, 6],
-            swap(self.route, 5, 9)
+            inverse(self.route, 5, 9)
         )
 
 
