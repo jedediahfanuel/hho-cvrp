974f3b4a492d75b195bb306917d043f3cdcd0c5a
Jeddi212 <jedediahf51@gmail.com>
2023-02-10 12:14:20 +0700

    🦢 :: mutate swap

--- optimizers/mutate.py
+++ optimizers/mutate.py
@@ -8,7 +8,7 @@ def inverse(r):
     zero to the length of the input list.
 
     :param r: one dimensional array-like
-    :return: one dimensional array-like after inversion
+    :return: one dimensional array-like after inverse mutation
 
     Examples
     --------
@@ -21,6 +21,29 @@ def inverse(r):
     i, j = random_indices(len(r))
 
     r[i:j + 1] = r[j:i - 1:-1] if i != 0 else r[j::-1]
+    return r
+
+
+def swap(r):
+    """
+    Inverse two edges, or the middle route.
+    Where i and j are randomly picked from
+    zero to the length of the input list.
+
+    :param r: one dimensional array-like
+    :return: one dimensional array-like after swap mutation
+
+    Examples
+    --------
+    >>> i, j = 2, 5
+    >>> A - B - F - E - D - C - G - H - A
+    >>>         |           |
+    >>>         |           |
+    >>> A - B - C - E - D - F - G - H - A
+    """
+    i, j = random_indices(len(r))
+
+    r[i], r[j] = r[j], r[i]
     return r
 
 
