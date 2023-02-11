c3bce53eee1f67edc6a9443d25a8cf2f914b1fe6
Jeddi212 <jedediahf51@gmail.com>
2023-02-11 18:10:53 +0700

    🍟 :: rearrange local search file

--- optimizers/local.py
+++ optimizers/local.py
@@ -1,3 +1,60 @@
+def insertion(r, i, j):
+    """
+    Perform insertion of a node.
+
+    :param r: one dimensional array-like
+    :param i: first point (index)
+    :param j: second point (index)
+    :return: one dimensional array-like after inversion
+
+    Examples
+    --------
+    >>> i, j = 1, 4
+    >>> A - B - E - D - C - F - G - H - A
+    >>> A - C - B - D - E - F - G - H - A
+    """
+    r[i+1:j+1], r[i] = r[i:j], r[j]
+    return r
+
+
+def inverse(r, i, j):
+    """
+    Inverse two edges, or the middle route.
+
+    :param r: one dimensional array-like
+    :param i: first point (index)
+    :param j: second point (index)
+    :return: one dimensional array-like after inversion
+
+    Examples
+    --------
+    >>> i, j = 1, 4
+    >>> A - B - E - D - C - F - G - H - A
+    >>> A - E - C - D - B - F - G - H - A
+    """
+    r[i:j + 1] = r[j:i-1:-1]
+    return r
+
+
+def swap(r, i, j):
+    """
+    Swap two edges, or the middle route.
+
+    :param r: one dimensional array-like
+    :param i: first point (index)
+    :param j: second point (index)
+    :return: one dimensional array-like after inversion
+
+    Examples
+    --------
+    >>> i, j = 1, 4
+    >>> A - B - E - D - C - F - G - H - A
+    >>> A - C - E - D - B - F - G - H - A
+    """
+    r[i], r[j] = r[j], r[i]
+    return r
+
+
 def two_opt_insertion(route, distances):
     """
     Two opt is a heuristic method that insert a node into another index.
@@ -28,25 +85,6 @@ def two_opt_insertion(route, distances):
     return route
 
 
-def insertion(r, i, j):
-    """
-    Perform insertion of a node.
-
-    :param r: one dimensional array-like
-    :param i: first point (index)
-    :param j: second point (index)
-    :return: one dimensional array-like after inversion
-
-    Examples
-    --------
-    >>> i, j = 1, 4
-    >>> A - B - E - D - C - F - G - H - A
-    >>> A - C - B - D - E - F - G - H - A
-    """
-    r[i+1:j+1], r[i] = r[i:j], r[j]
-    return r
-
-
 def two_opt_inverse(route, distances):
     """
     Two opt is a heuristic method that inverse two edges in a graph.
@@ -71,25 +109,6 @@ def two_opt_inverse(route, distances):
                     improved = True
 
     return route
-
-
-def inverse(r, i, j):
-    """
-    Inverse two edges, or the middle route.
-
-    :param r: one dimensional array-like
-    :param i: first point (index)
-    :param j: second point (index)
-    :return: one dimensional array-like after inversion
-
-    Examples
-    --------
-    >>> i, j = 1, 4
-    >>> A - B - E - D - C - F - G - H - A
-    >>> A - B - C - D - E - F - G - H - A
-    """
-    r[i:j + 1] = r[j:i-1:-1]
-    return r
 
 
 def two_opt_swap(route, distances):
@@ -122,22 +141,3 @@ def two_opt_swap(route, distances):
                     improved = True
 
     return route
-
-
-def swap(r, i, j):
-    """
-    Swap two edges, or the middle route.
-
-    :param r: one dimensional array-like
-    :param i: first point (index)
-    :param j: second point (index)
-    :return: one dimensional array-like after inversion
-
-    Examples
-    --------
-    >>> i, j = 1, 4
-    >>> A - B - E - D - C - F - G - H - A
-    >>> A - C - E - D - B - F - G - H - A
-    """
-    r[i], r[j] = r[j], r[i]
-    return r
