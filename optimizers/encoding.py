2956eba980d72cbb36881365d78030a9935c32b7
Jeddi212 <jedediahf51@gmail.com>
2023-02-15 15:47:41 +0700

    🔛 :: random key encoding original sequence

--- optimizers/encoding.py
+++ optimizers/encoding.py
@@ -1,13 +1,13 @@
 def random_key(arr):
     """
-    This function takes a 1-dimensional list or array as input
-    and returns  a list of indices that correspond to the sorted
-    elements in the original list or array. The returned list of indices
-    can be used to access the sorted elements in the original list or array.
-    The sorting is performed in ascending order on the values of the elements.
+    Return a list of indices that correspond to the sorted elements in the input list.
 
-    :param arr: 1-dimensional array-like
-    :return: 1-dimensional array-like
+    The function takes a 1-dimensional list or array of real numbers as input and returns a list of indices that
+    correspond to the sorted elements. The returned list of indices can be used to access the sorted elements in the
+    input list.
+
+    :param arr: A 1-dimensional list or array of real numbers.
+    :return: A list of indices that correspond to the sorted elements in the input list.
 
     Examples
     --------
@@ -16,4 +16,28 @@ def random_key(arr):
     [1, 5, 3, 2, 4]
 
     """
-    return [i + 1 for i, x in sorted(enumerate(arr), key=lambda x: x[1])]
+    return [i + 1 for i, _ in sorted(enumerate(arr), key=lambda x: x[1])]
+
+
+def random_key_original(arr):
+    """
+    Return a list of indices that correspond to the sorted elements in the original sequence.
+
+    The function takes a 1-dimensional list or array of real numbers as input and returns a list of indices that
+    correspond to the sorted elements in the original sequence. The returned list of indices can be used to access
+    the sorted elements in the original sequence.
+
+    :param arr: A 1-dimensional list or array of real numbers.
+    :return: A list of indices that correspond to the sorted elements in the original sequence.
+
+    Examples
+    --------
+    >>> ary = [0.1, 0.5, 0.3, 0.7, 0.2]
+    >>> print(random_key(ary))
+    [1, 4, 3, 5, 2]
+
+    """
+    indices = list(range(len(arr)))
+    indices.sort(key=lambda i: arr[i])
+    return [i + 1 for i, _ in sorted(enumerate(indices), key=lambda x: x[1])]
+
--- test/encoding_test.py
+++ test/encoding_test.py
@@ -1,17 +1,25 @@
 import unittest
 
 from optimizers.encoding import random_key
+from optimizers.encoding import random_key_original
 
 
 class EncodingTest(unittest.TestCase):
     def setUp(self):
         self.arr = [0.1, 0.5, 0.3, 0.7, 0.2]
         self.res = [1, 5, 3, 2, 4]
+        self.res_original = [1, 4, 3, 5, 2]
 
     def test_random_key(self):
         self.assertEqual(
             self.res,
             random_key(self.arr)
+        )
+
+    def test_random_key_original(self):
+        self.assertEqual(
+            self.res_original,
+            random_key_original(self.arr)
         )
 
 
