3683f273a5d03d6127ebcaa022e922256cf72e3b
Jeddi212 <jedediahf51@gmail.com>
2023-02-15 20:31:43 +0700

    🧪 :: random key test

--- test/encode_test.py
+++ test/encode_test.py
@@ -7,18 +7,18 @@ from optimizers.encode import random_key
 class EncodeTest(unittest.TestCase):
     def setUp(self):
         self.arr = [0.1, 0.5, 0.3, 0.7, 0.2]
-        self.res = [1, 5, 3, 2, 4]
-        self.res_original = [1, 4, 3, 5, 2]
-
-    def test_random_key(self):
-        self.assertEqual(
-            self.res,
-            random_key_original(self.arr)
-        )
+        self.result_original = [1, 5, 3, 2, 4]
+        self.result = [1, 4, 3, 5, 2]
 
     def test_random_key_original(self):
         self.assertEqual(
-            self.res_original,
+            self.result_original,
+            random_key_original(self.arr)
+        )
+
+    def test_random_key(self):
+        self.assertEqual(
+            self.result,
             random_key(self.arr)
         )
 
