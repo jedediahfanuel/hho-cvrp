ae903d6d03fbb47d118aa7a87e705a59a5ce2649
Jeddi212 <jedediahf51@gmail.com>
2023-03-05 17:03:34 +0700

    🏜 :: iter & pop input field

---
+++ main.py
@@ -0,0 +1,62 @@
+import tkinter as tk
+
+
+class IntegerInputField(tk.Entry):
+    """Custom Entry widget that only accepts integer input."""
+
+    def __init__(self, master=None, **kwargs):
+        super().__init__(master, **kwargs)
+        self.configure(validate='key')
+        self.configure(validatecommand=(self.register(self.validate_input), '%P'))
+
+    @staticmethod
+    def validate_input(value):
+        if value.isdigit():
+            return True
+        elif value == '' or value == '-':
+            return True
+        else:
+            return False
+
+
+def validate_button(*args):
+    # Check if input values are valid integers greater than or equal to 1
+    if iteration_input.get().isdigit() \
+            and population_input.get().isdigit() \
+            and int(iteration_input.get()) >= 1 \
+            and int(population_input.get()) >= 1:
+        submit_button.config(state='normal')
+    else:
+        submit_button.config(state='disabled')
+
+
+def submit_button_callback():
+    # Handle input values
+    print(f"Iteration: {iteration_input.get()}")
+    print(f"Population: {population_input.get()}")
+
+
+# Create main window
+root = tk.Tk()
+root.title("Integer Input Fields")
+
+# Create input fields
+iteration_label = tk.Label(root, text="Iteration:")
+iteration_input = IntegerInputField(root)
+iteration_input.bind('<KeyRelease>', validate_button)
+population_label = tk.Label(root, text="Population:")
+population_input = IntegerInputField(root)
+population_input.bind('<KeyRelease>', validate_button)
+
+# Create submit button
+submit_button = tk.Button(root, text="Submit", command=submit_button_callback, state='disabled')
+
+# Arrange widgets in grid
+iteration_label.grid(row=0, column=0, padx=5, pady=5, sticky='E')
+iteration_input.grid(row=0, column=1, padx=5, pady=5, sticky='W')
+population_label.grid(row=1, column=0, padx=5, pady=5, sticky='E')
+population_input.grid(row=1, column=1, padx=5, pady=5, sticky='W')
+submit_button.grid(row=2, column=1, padx=5, pady=5, sticky='E')
+
+# Run main event loop
+root.mainloop()
