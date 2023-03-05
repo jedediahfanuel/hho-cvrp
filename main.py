7e59c946860083ed894f84fe31554372b3617f94
Jeddi212 <jedediahf51@gmail.com>
2023-03-05 17:37:18 +0700

    💡 :: validate selected instance

--- main.py
+++ main.py
@@ -20,12 +20,15 @@ class IntegerInputField(tk.Entry):
             return False
 
 
-def validate_button(*args):
+def validate_inputs(*args):
     # Check if input values are valid integers greater than or equal to 1
     if iteration_input.get().isdigit() \
             and population_input.get().isdigit() \
+            and nruns_input.get().isdigit() \
             and int(iteration_input.get()) >= 1 \
-            and int(population_input.get()) >= 1:
+            and int(population_input.get()) >= 1 \
+            and int(nruns_input.get()) >= 1\
+            and items_listbox.curselection():
         submit_button.config(state='normal')
     else:
         submit_button.config(state='disabled')
@@ -35,30 +38,36 @@ def submit_button_callback():
     # Handle input values
     print(f"Iteration: {iteration_input.get()}")
     print(f"Population: {population_input.get()}")
+    print(f"Num of run: {nruns_input.get()}")
     selected_items = [items_listbox.get(idx) for idx in items_listbox.curselection()]
     print(f"Selected Items: {selected_items}")
 
 
 # Create main window
 root = tk.Tk()
-root.title("Integer Input Fields")
+root.title("HHO-CVRP")
 root.configure(bg="white")
 
 # Create input fields
 iteration_label = tk.Label(root, text="Iteration:", bg="white")
 iteration_input = IntegerInputField(root)
-iteration_input.bind('<KeyRelease>', validate_button)
+iteration_input.bind('<KeyRelease>', validate_inputs)
 
 population_label = tk.Label(root, text="Population:", bg="white")
 population_input = IntegerInputField(root)
-population_input.bind('<KeyRelease>', validate_button)
+population_input.bind('<KeyRelease>', validate_inputs)
+
+nruns_label = tk.Label(root, text="Num of run:", bg="white")
+nruns_input = IntegerInputField(root)
+nruns_input.bind('<KeyRelease>', validate_inputs)
 
 # Create dropdown menu with checkboxes
 items = cvrplib.list_names(vrp_type='cvrp')
+items_label = tk.Label(root, text="Instances:", bg="white")
 items_listbox = tk.Listbox(root, selectmode=tk.MULTIPLE, exportselection=False)
 for item in items:
     items_listbox.insert(tk.END, item)
-items_label = tk.Label(root, text="Instances:", bg="white")
+items_listbox.bind('<<ListboxSelect>>', validate_inputs)
 
 # Create submit button
 submit_button = tk.Button(root, text="Submit", command=submit_button_callback, state='disabled')
@@ -66,11 +75,17 @@ submit_button = tk.Button(root, text="Submit", command=submit_button_callback, s
 # Arrange widgets in grid
 iteration_label.grid(row=0, column=0, padx=5, pady=5, sticky='E')
 iteration_input.grid(row=0, column=1, padx=5, pady=5, sticky='W')
+
 population_label.grid(row=1, column=0, padx=5, pady=5, sticky='E')
 population_input.grid(row=1, column=1, padx=5, pady=5, sticky='W')
-items_label.grid(row=2, column=0, padx=5, pady=5, sticky='E')
-items_listbox.grid(row=2, column=1, padx=5, pady=5, sticky='W')
-submit_button.grid(row=3, column=1, padx=5, pady=5, sticky='E')
+
+nruns_label.grid(row=2, column=0, padx=5, pady=5, sticky='E')
+nruns_input.grid(row=2, column=1, padx=5, pady=5, sticky='W')
+
+items_label.grid(row=3, column=0, padx=5, pady=5, sticky='E')
+items_listbox.grid(row=3, column=1, padx=5, pady=5, sticky='W')
+
+submit_button.grid(row=4, column=1, padx=5, pady=5, sticky='E')
 
 # Run main event loop
 root.mainloop()
