5b00265a88bbd75608c95a751310e115666ddad1
Jeddi212 <jedediahf51@gmail.com>
2023-03-14 14:01:18 +0700

    🎓 :: export class

---
+++ model/export.py
@@ -0,0 +1,9 @@
+class Export:
+    def __init__(self, avg, detail, conv, box, scatter, route, config):
+        export_avg = avg
+        export_details = detail
+        export_convergence = conv
+        export_boxplot = box
+        export_scatter = scatter
+        export_route = route
+        export_configuration = config
