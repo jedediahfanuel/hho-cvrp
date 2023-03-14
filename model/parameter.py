765809467c253df80d604fa2d53cabecb54eadae
Jeddi212 <jedediahf51@gmail.com>
2023-03-15 02:12:22 +0700

    🌇 :: default result directory name

--- model/parameter.py
+++ model/parameter.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 
 class Parameter:
-    results_directory = "out/" + time.strftime("%Y-%m-%d-%H-%M-%S") + "/"
+    results_directory = "hho_cvrp/" + time.strftime("%Y-%m-%d-%H-%M-%S") + "/"
     optimizers = ["HHO"]
 
     def __init__(self, n_runs, population, iteration, instances, results_directory="", optimizers=None):
@@ -16,8 +16,8 @@ class Parameter:
             self.optimizers = optimizers
 
         if results_directory != "":
-            self.results_directory = results_directory + time.strftime("%Y-%m-%d-%H-%M-%S") + "/"
+            self.results_directory = results_directory + "/hho_cvrp/" + time.strftime("%Y-%m-%d-%H-%M-%S") + "/"
         else:
-            self.results_directory = "out/" + time.strftime("%Y-%m-%d-%H-%M-%S") + "/"
+            self.results_directory = "hho_cvrp/" + time.strftime("%Y-%m-%d-%H-%M-%S") + "/"
 
         Path(self.results_directory).mkdir(parents=True, exist_ok=True)
