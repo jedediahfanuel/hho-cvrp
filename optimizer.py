958c841b4cc9c29a7d75594f9973413c1079a36b
Jeddi212 <jedediahf51@gmail.com>
2023-03-14 13:55:33 +0700

    🦑 :: brings out write detail

--- optimizer.py
+++ optimizer.py
@@ -7,10 +7,12 @@ import numpy
 
 import benchmarks
 from optimizers.hho_cvrp import hho
+from model.collection import Collection
 import plot_boxplot as box_plot
 import plot_convergence as conv_plot
 import plot_scatter as scatter_plot
 import write_configuration
+import write_details
 import write_routes
 
 warnings.simplefilter(action="ignore")
@@ -92,7 +94,6 @@ def run(optimizer, instances, num_of_runs, params: dict[str, int], export_flags:
     export_configuration = export_flags["export_configuration"]
 
     flag = False
-    flag_details = False
 
     # CSV Header for the convergence
     cnvg_header = []
@@ -105,56 +106,27 @@ def run(optimizer, instances, num_of_runs, params: dict[str, int], export_flags:
 
     for i in range(0, len(optimizer)):
         for j in range(0, len(instances)):
-            convergence = [0] * num_of_runs
-            execution_time = [0] * num_of_runs
-            best_solution = [0] * num_of_runs
-            gap_solution = [0] * num_of_runs
+            collection = Collection(num_of_runs)
             for k in range(0, num_of_runs):
                 func_details = benchmarks.get_function_details(instances[j])
-                x = selector(optimizer[i], func_details, population_size, iterations)
-                convergence[k] = x.convergence
+                solution = selector(optimizer[i], func_details, population_size, iterations)
+                collection.convergence[k] = solution.convergence
 
                 if export_details:
                     export_to_file = results_directory + "experiment_details.csv"
-                    with open(export_to_file, "a", newline="\n") as out:
-                        writer = csv.writer(out, delimiter=",")
-                        if not flag_details:
-                            # just one time to write the header of the CSV file
-                            header = numpy.concatenate(
-                                [["Optimizer", "Instance", "BKS", "BS", "Gap", "ExecutionTime"], cnvg_header]
-                            )
-                            writer.writerow(header)
-                            flag_details = True  # at least one experiment
-                        execution_time[k] = x.execution_time
-                        best_solution[k] = x.best
-                        gap_solution[k] = benchmarks.gap(x.bks, x.best)
-                        a = numpy.concatenate(
-                            [
-                                [
-                                    x.optimizer,
-                                    x.name,
-                                    x.bks,
-                                    x.best,
-                                    gap_solution[k],
-                                    x.execution_time
-                                ],
-                                x.convergence
-                            ]
-                        )
-                        writer.writerow(a)
-                    out.close()
+                    write_details.run(export_to_file, collection, solution, cnvg_header, k)
 
                 if export_route:
-                    rd = results_directory + "routes-" + x.optimizer + "/" + x.name + "/"
+                    rd = results_directory + "routes-" + solution.optimizer + "/" + solution.name + "/"
                     Path(rd).mkdir(parents=True, exist_ok=True)
-                    write_routes.run(x, rd, k)
+                    write_routes.run(solution, rd, k)
 
                 if export_scatter:
-                    close = "/" if x.coordinates is not None else "/None"
-                    rd = results_directory + "scatter-plot-" + x.optimizer + "/" + x.name + close
+                    close = "/" if solution.coordinates is not None else "/None"
+                    rd = results_directory + "scatter-plot-" + solution.optimizer + "/" + solution.name + close
                     Path(rd).mkdir(parents=True, exist_ok=True)
 
-                    scatter_plot.run(x, rd, k) if x.coordinates is not None else ()
+                    scatter_plot.run(solution, rd, k) if solution.coordinates is not None else ()
 
             if export:
                 export_to_file = results_directory + "experiment_avg.csv"
@@ -169,18 +141,18 @@ def run(optimizer, instances, num_of_runs, params: dict[str, int], export_flags:
                         writer.writerow(header)
                         flag = True
 
-                    avg_execution_time = float("%0.2f" % (sum(execution_time) / num_of_runs))
-                    avg_bs = float("%0.2f" % (sum(best_solution) / num_of_runs))
-                    avg_gap = float("%0.4f" % (sum(gap_solution) / num_of_runs))
+                    avg_execution_time = float("%0.2f" % (sum(collection.execution_time) / num_of_runs))
+                    avg_bs = float("%0.2f" % (sum(collection.best_solution) / num_of_runs))
+                    avg_gap = float("%0.4f" % (sum(collection.gap_solution) / num_of_runs))
                     avg_convergence = numpy.around(
-                        numpy.mean(convergence, axis=0, dtype=numpy.float64), decimals=2
+                        numpy.mean(collection.convergence, axis=0, dtype=numpy.float64), decimals=2
                     ).tolist()
                     a = numpy.concatenate(
                         [
                             [
-                                x.optimizer,
-                                x.name,
-                                x.bks,
+                                solution.optimizer,
+                                solution.name,
+                                solution.bks,
                                 avg_bs,
                                 avg_gap,
                                 avg_execution_time
