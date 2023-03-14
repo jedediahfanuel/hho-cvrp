a1ccc8c53ca2109105954fe34f730164dd1d470f
Jeddi212 <jedediahf51@gmail.com>
2023-03-14 20:52:54 +0700

    📮 :: undo sep depot creation

--- plot_scatter.py
+++ plot_scatter.py
@@ -1,6 +1,6 @@
 import platform
 from pathlib import Path
-from solution import Solution
+from model.solution import Solution
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -31,7 +31,7 @@ def run(s: Solution, results_directory, k: int):
 
     plt.ioff()
     fn = "scatter-" + s.name + "-" + str(k) + "-" + str(s.best)
-    plot_cities(s, pathsave=results_directory, filename=fn, size=50, show_id=True)
+    plot_cities(s, pathsave=results_directory, filename=fn, size=10, show_id=False)
 
 
 def get_space(coordinates):
@@ -62,12 +62,8 @@ def plot_cities(s: Solution, filename: str, pathsave: str, exts=(".png", ".pdf")
         line_y = np.array([coordinates[y, 1] for y in r])
 
         # draw customer & routes
-        plt.scatter(point_x[1:-1], point_y[1:-1], s=size)
+        plt.scatter(point_x[:-1], point_y[:-1], s=size)
         plt.plot(line_x, line_y, '-', label=str(f'route {i + 1}'), lw=0.5)
-
-        # draw depot
-        plt.scatter(point_x[0], point_y[0], s=size, marker='^')
-        plt.plot(coordinates[0, 0], coordinates[0, 1])  # dummy line for color correction
 
         plt.text(x_min - 2 * space_x, y_min - 2 * space_y, f"Total distance: {s.best}",
                  fontdict={'size': 8, 'color': 'red'})
