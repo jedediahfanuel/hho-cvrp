964091cacf311981eae834efd74920c418806bc1
Jeddi212 <jedediahf51@gmail.com>
2023-03-14 14:08:27 +0700

    🐹 :: remove extra export_name

--- model/export.py
+++ model/export.py
@@ -1,9 +1,9 @@
 class Export:
     def __init__(self, avg, detail, conv, box, scatter, route, config):
-        self.export_avg = avg
-        self.export_details = detail
-        self.export_convergence = conv
-        self.export_boxplot = box
-        self.export_scatter = scatter
-        self.export_route = route
-        self.export_configuration = config
+        self.avg = avg
+        self.details = detail
+        self.convergence = conv
+        self.boxplot = box
+        self.scatter = scatter
+        self.route = route
+        self.configuration = config
