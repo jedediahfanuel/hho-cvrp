7eec0167ffe1efe52d2415afa24a03ad2ee8d48a
Jeddi212 <jedediahf51@gmail.com>
2023-03-15 12:21:22 +0700

    🦀 :: remove unused rke

--- method/encode.py
+++ method/encode.py
@@ -1,24 +1,3 @@
-def random_key_original(arr):
-    """
-    Return a list of indices that correspond to the sorted elements in the input list.
-
-    The function takes a 1-dimensional list or array of real numbers as input and returns a list of indices that
-    correspond to the sorted elements. The returned list of indices can be used to access the sorted elements in the
-    input list.
-
-    :param arr: A 1-dimensional list or array of real numbers.
-    :return: A list of indices that correspond to the sorted elements in the input list.
-
-    Examples
-    --------
-    >>> ary = [0.1, 0.5, 0.3, 0.7, 0.2]
-    >>> print(random_key_original(ary))
-    [1, 5, 3, 2, 4]
-
-    """
-    return [i + 1 for i, _ in sorted(enumerate(arr), key=lambda x: x[1])]
-
-
 def random_key(arr):
     """
     Return a list of indices that correspond to the sorted elements in the original sequence.
