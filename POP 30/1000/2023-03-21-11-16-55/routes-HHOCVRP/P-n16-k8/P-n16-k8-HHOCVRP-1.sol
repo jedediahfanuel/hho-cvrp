ec283a8a76a98f0e6547c773a01bbc20bc2f7874
Jeddi212 <jedediahf51@gmail.com>
2023-03-23 11:36:47 +0700

    🌁 :: remove unused 30 1000

--- POP 30/1000/2023-03-21-11-16-55/routes-HHOCVRP/P-n16-k8/P-n16-k8-HHOCVRP-1.sol
+++
@@ -1,9 +0,0 @@
-Route #1: 6
-Route #2: 14 5
-Route #3: 7 9 13
-Route #4: 1 3
-Route #5: 2
-Route #6: 8
-Route #7: 10 12 15
-Route #8: 11 4
-Cost 450
--- POP 30/1000/2023-03-21-11-16-55/routes-HHOCVRP/P-n16-k8/P-n16-k8-HHOCVRP-3.sol
+++
@@ -1,9 +0,0 @@
-Route #1: 6
-Route #2: 5 14
-Route #3: 7 9 13
-Route #4: 2
-Route #5: 8
-Route #6: 3 1
-Route #7: 15 12 10
-Route #8: 4 11
-Cost 450
