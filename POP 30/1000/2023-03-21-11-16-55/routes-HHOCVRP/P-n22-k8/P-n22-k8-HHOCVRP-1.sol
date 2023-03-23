8553aa05d7da2d44c972e600d1ee1dea5f39e5f6
Jedediah Fanuel <60323767+Jeddi212@users.noreply.github.com>
2023-03-23 04:29:33 +0000

    Revert "💳 :: remove unused directory pop 30 1000"
    This reverts commit 0b136af72e37fc8df90d9cb8a6cf73deb8947057.

---
+++ POP 30/1000/2023-03-21-11-16-55/routes-HHOCVRP/P-n22-k8/P-n22-k8-HHOCVRP-1.sol
@@ -0,0 +1,10 @@
+Route #1: 11 13
+Route #2: 10 3 4 8
+Route #3: 6 1 2 9
+Route #4: 5 7
+Route #5: 12 15
+Route #6: 18 20 14
+Route #7: 16
+Route #8: 19
+Route #9: 21 17
+Cost 600
---
+++ POP 30/1000/2023-03-21-11-16-55/routes-HHOCVRP/P-n22-k8/P-n22-k8-HHOCVRP-4.sol
@@ -0,0 +1,10 @@
+Route #1: 14 17 21
+Route #2: 19
+Route #3: 18 20
+Route #4: 15 12
+Route #5: 16
+Route #6: 5 7
+Route #7: 6 1 2 9
+Route #8: 10 3 4 8
+Route #9: 11 13
+Cost 600
